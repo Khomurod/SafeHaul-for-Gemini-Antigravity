1d5b18bfc52263271e27097e262d3f78d8382924
Khomurod <46620174+Khomurod@users.noreply.github.com>
2026-02-15 23:36:57 +0500

    some changes

--- scripts/verify_functions_load.js
+++
@@ -1,7 +0,0 @@
-try {
-    const integrations = require('./integrations/index.js');
-    console.log("Successfully loaded integrations/index.js");
-} catch (error) {
-    console.error("FAILED to load integrations/index.js");
-    console.error(error);
-}
